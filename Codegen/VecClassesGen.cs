5bf4d117ee17636f624af8e795559dad5d4cb8a7
Yotic <55879406+Yoticc@users.noreply.github.com>
2023-09-28 11:01:25 +0300

    Update VecClassesGen.cs

--- Codegen/VecClassesGen.cs
+++ Codegen/VecClassesGen.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static System.Linq.Enumerable;
+using static System.String;
 using System.Threading.Tasks;
 
 namespace Codegen;
 public class VecClassesGen
 {
-    public static void Exec(int count = 16)
+    public static string Exec(int count = 16) => Join("\n", Range(2, count).Select(i =>
     {
-        Console.WriteLine(string.Join("\n", Enumerable.Range(2, count).Select(i =>
-        {
-            var genericTypes = $"<{string.Join(", ", Enumerable.Range(0, i).Select(GenType))}>";
-            var properties = string.Join(", ", Enumerable.Range(0, i).Select(index => $"{GenType(index)} {GenPropertyName(index)}"));
-            var interfaces = string.Join('\n', Enumerable.Range(0, i).Select(index => $"        where {GenType(index)} : unmanaged"));
-            var methArgs = string.Join(", ", Enumerable.Range(0, i).Select(index => $"{GetType(index)} {GetArgName(index)}"));
-            var ctorArgs = string.Join(", ", Enumerable.Range(0, i).Select(index => $"a.{GetArgName(index)}"));
-            var ctorPtrArgs = string.Join(", ", Enumerable.Range(0, i).Select(index => $"a->{GetPropertyName(index)}"));
-            var anonArgs = string.Join(", ", Enumerable.Range(0, i).Select(index => $"a.{GetPropertyName(index)}"));
-            return 
+        var genericTypes = $"<{Join(", ", Range(0, i).Select(GenType))}>",
+            properties = string.Join(", ", Range(0, i).Select(index => $"{GenType(index)} {GenPropertyName(index)}")),
+            interfaces = string.Join('\n', Range(0, i).Select(index => $"        where {GenType(index)} : unmanaged")),
+            methArgs = string.Join(", ", Range(0, i).Select(index => $"{GetType(index)} {GetArgName(index)}")),
+            ctorArgs = string.Join(", ", Range(0, i).Select(index => $"a.{GetArgName(index)}")),
+            ctorPtrArgs = string.Join(", ", Range(0, i).Select(index => $"a->{GetPropertyName(index)}")),
+            anonArgs = string.Join(", ", Range(0, i).Select(index => $"a.{GetPropertyName(index)}"));
+        return 
 $@"
 public unsafe record struct Vec{genericTypes}({properties})
 {interfaces}
@@ -28,9 +28,8 @@ public unsafe record struct Vec{genericTypes}({properties})
     public static explicit operator ({methArgs})({genericTypes} a) => ({anonArgs});
 }}
 ";
-            string GenType(int index) => index == 0 ? "T" : $"T{index}";
-            string GenPropertyName(int index) => index == 0 ? "_" : $"_{index}";
-            string GenArgName(int index) => $"a{index}";
-        })));
-    }
+        string GenType(int index) => index == 0 ? "T" : $"T{index}";
+        string GenPropertyName(int index) => index == 0 ? "_" : $"_{index}";
+        string GenArgName(int index) => $"a{index}";
+    })));
 }
